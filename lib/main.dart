9cc1fa22b5dd899134cacd8816817db49776d39e
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2025-01-13 14:49:12 +0900

    ナビゲーションを追加

--- lib/main.dart
+++ lib/main.dart
@@ -48,8 +48,76 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+// 先頭に_をつけるとプライベートになる
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
   // widgetは必ずbuildメソッドをもつ。widget or widgetのネストしたツリーを返す
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        // Placeholderは仮置きのwidget
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    // LayoutBuilderは、親ウィジェットのサイズに応じてレイアウトを変更するウィジェット
+    return LayoutBuilder(builder: (context, constraints) {
+      // Scaffoldは、アプリケーションの基本的な構造を提供するウィジェット（？）
+      return Scaffold(
+        body: Row(
+          children: [
+            // SafeAreaでノッチとかを考慮してくれる
+            SafeArea(
+              child: NavigationRail(
+                // extended: falseだとアイコンのみ、trueだとラベルも表示する
+                extended: constraints.maxWidth > 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  // Stateの内部ではsetState()をつかえる
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            // Expandedで、余った幅をとりにいく
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
@@ -63,37 +131,33 @@ class MyHomePage extends StatelessWidget {
       icon = Icons.favorite_border;
     }
 
-    // トップレベルのwidgetがscaffold
-    return Scaffold(
-      // columnは任意の数のchildrenを持ち、上から下に配置する
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BigCard(pair: pair),
-            SizedBox(height: 20),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                // ElevatedButton.icon で icon: ... を指定するとアイコン付きのボタンになる
-                ElevatedButton.icon(
-                  onPressed: () {
-                    appState.toggleFavorite();
-                  },
-                  icon: Icon(icon),
-                  label: Text('Like'),
-                ),
-                SizedBox(width: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    appState.getNext();
-                  },
-                  child: Text('Next'),
-                ),
-              ],
-            ),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // ElevatedButton.icon で icon: ... を指定するとアイコン付きのボタンになる
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
