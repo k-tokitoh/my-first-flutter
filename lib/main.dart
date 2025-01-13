999a67e4455bd7d18287a6ed693ee13d715b33b7
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2025-01-13 14:10:53 +0900

    スタイルを修正

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         ),
         home: MyHomePage(),
       ),
@@ -42,21 +42,63 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     // トップレベルのwidgetがscaffold
     return Scaffold(
       // columnは任意の数のchildrenを持ち、上から下に配置する
-      body: Column(
-        children: [
-          Text('A random idea: hoge'),
-          Text(appState.current.asLowerCase),
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    // 現在のcontextのテーマを取得
+    final theme = Theme.of(context);
+
+    // displayは見出しの意味
+    // copyWith()で、引数での指定のみ変更されたテキストスタイルを表現するオブジェクトのコピーが返る
+    final style = theme.textTheme.displayMedium!.copyWith(
+      // primary colorの上に適した色
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      // テーマのプライマリカラーを背景色に設定
+      color: theme.colorScheme.primary,
+      // 浮き上がり = 影の深さ
+      elevation: 8,
+      child: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          // screen readerに読み上げ対象として渡されるラベル
+          semanticsLabel: '${pair.first} ${pair.second}',
+        ),
       ),
     );
   }
