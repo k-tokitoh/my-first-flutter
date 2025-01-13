ad2cdfe6eda0ddf9617c70c08a191b56c4082b28
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2025-01-13 13:44:09 +0900

    ボタンを追加

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,14 @@ void main() {
   runApp(MyApp());
 }
 
+// ウィジェットは、すべての Flutter アプリを作成する際の元になる要素です。ご覧のように、このアプリ自体がウィジェットです。
+// StatelessWidget, StatefulWidget がある
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Flutter には、アプリの状態を管理する強力な方法が多数あります。特に説明しやすいのが、このアプリで採用している ChangeNotifier を使用する方法です。
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
@@ -27,18 +30,32 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
+  // widgetは必ずbuildメソッドをもつ。widget or widgetのネストしたツリーを返す
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
+    // トップレベルのwidgetがscaffold
     return Scaffold(
+      // columnは任意の数のchildrenを持ち、上から下に配置する
       body: Column(
         children: [
-          Text('A random idea:'),
+          Text('A random idea: hoge'),
           Text(appState.current.asLowerCase),
+          ElevatedButton(
+            onPressed: () {
+              appState.getNext();
+            },
+            child: Text('Next'),
+          ),
         ],
       ),
     );
